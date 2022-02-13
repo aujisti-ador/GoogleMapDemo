c816c4cbf6f59ba6b296749572edfd4650a1dc3f
Fazle Rabbi Ador <ador.fazlerabbi@gmail.com>
2022-02-13 11:41:04 +0600

    Current location added

--- App.js
+++ App.js
@@ -1,39 +1,144 @@
 // Integration of Google map in React Native using react-native-maps
 // https://aboutreact.com/react-native-map-example/
 
-// Import React
-import React, { useState } from 'react';
-// Import required components
+import React, { useEffect, useState } from 'react';
 import { SafeAreaView, StyleSheet, View } from 'react-native';
-
-// Import Map and Marker
 import MapView, { Marker } from 'react-native-maps';
+import RNLocation from 'react-native-location';
 
 const App = () => {
 
   const [marker, setMarker] = useState(
     {
-      latitude: 20.6230175079,
-      longitude: 92.3202120525
+      latitude: 0,
+      longitude: 0
     })
+
+  let _startUpdatingLocation = () => {
+    console.log('in start updating location')
+    // RNLocation.subscribeToLocationUpdates(
+    //   locations => {
+    //     // this.setState({ location: locations[0] });
+    //     console.log('here')
+    //     console.log(locations)
+
+    //     setMarker({
+    //       latitude: locations[0].latitude,
+    //       longitude: locations[0].longitude
+    //     })
+    //   }
+    // )
+
+    // RNLocation.getLatestLocation({ timeout: 60000 })
+    //   .then(latestLocation => {
+    //     // Use the location here
+    //     console.log(latestLocation)
+
+    //     setMarker({
+    //       latitude: locations[0].latitude,
+    //       longitude: locations[0].longitude
+    //     })
+    //   })
+  }
+
+  useEffect(() => {
+    // Geolocation.getCurrentPosition(position => {
+    //   const initialPosition = JSON.stringify(position)
+    //   // console.warn(initialPosition)
+    //   // console.warn(position.coords.latitude)
+    //   // console.warn(position.coords.longitude)
+    //   setMarker({
+    //     latitude: position.coords.latitude,
+    //     longitude: position.coords.longitude
+    //   })
+
+    // })
+
+    RNLocation.configure({
+      distanceFilter: 100, // Meters
+      desiredAccuracy: {
+        ios: "best",
+        android: "balancedPowerAccuracy"
+      },
+      // Android only
+      androidProvider: "auto",
+      interval: 5000, // Milliseconds
+      fastestInterval: 10000, // Milliseconds
+      maxWaitTime: 5000, // Milliseconds
+    })
+
+    // RNLocation.checkPermission({
+    //   ios: 'whenInUse', // or 'always'
+    //   android: {
+    //     detail: 'coarse' // or 'fine'
+    //   }
+    // }).then((res) => {
+    //   console.log(res)
+    // })
+
+    // RNLocation.getLatestLocation({ timeout: 60000 })
+    //   .then(latestLocation => {
+    //     // Use the location here
+    //     console.log(latestLocation)
+
+    //     setMarker({
+    //       latitude: locations[0].latitude,
+    //       longitude: locations[0].longitude
+    //     })
+    //   })
+
+
+    RNLocation.requestPermission({
+      ios: "whenInUse",
+      android: {
+        detail: "coarse",
+        rationale: {
+          title: "We need to access your location",
+          message: "We use your location to show where you are on the map",
+          buttonPositive: "OK",
+          buttonNegative: "Cancel"
+        }
+      }
+    }).then(granted => {
+      if (granted) {
+        // console.log(granted)
+
+        // _startUpdatingLocation()
+
+        RNLocation.getLatestLocation({ timeout: 60000 })
+          .then(latestLocation => {
+            // Use the location here
+            console.log(latestLocation)
+
+            setMarker({
+              latitude: 37.42342342342342,
+              longitude: -122.08395287867832
+            })
+          })
+
+        console.log('----', marker)
+      }
+    })
+  })
 
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <View style={styles.container}>
         <MapView
           style={styles.mapStyle}
-          initialRegion={{
-            latitude: 20.6230175079,
-            longitude: 92.3202120525,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421,
+          // region={{
+          //   latitude: marker.latitude,
+          //   longitude: marker.longitude,
+          //   latitudeDelta: 0.0922,
+          //   longitudeDelta: 0.0421,
+          // }}
+
+          showsUserLocation={true}
+          followsUserLocation={true}
+          loadingEnabled={true}
+          onMapReady={() => {
+
           }}
-          // customMapStyle={mapStyle}
-          onPress={(e) => {
-            setMarker(e.nativeEvent.coordinate)
-            alert(JSON.stringify(e.nativeEvent.coordinate))
-          }
-          }
         >
           <Marker
             draggable
